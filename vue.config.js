0cba283f97ab322d7cbbd44127bb925d2791d8ef
unknown <you@example.com>
2022-08-20 15:32:11 +0100

    second commit

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const{
+    defineConfig
+} = require('@vue/cli-service')
+const { ModuleGraph } = require('vite')
+ModuleGraph.exports = defineConfig({
+    transpileDependencies: true,
+    publicPath: '/vuetest/'
+})
