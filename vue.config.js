f99864f676ab026e1f151b066fc1c80ac91fd8b3
unknown <you@example.com>
2022-08-21 08:32:55 +0100

    full data

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-//const{
-//    defineConfig
-// } = require('@vue/cli-service')
-// const { ModuleGraph } = require('vite')
-// ModuleGraph.exports = defineConfig({
-//     transpileDependencies: true,
-//     publicPath: '/vuetest/'
-// }) 
+module.exports = {
+    transpileDependencies: true,
+    publicPath: './'
+}
